2da85b872cb685a5b49689e7c2d21752409bba98
mohamedhamdy9966 <mh7008002@gmail.com>
2026-04-24 16:56:34 +0300

    Revert "Merge pull request #159 from mohamedhamdy9966/development fix es lint"
    This reverts commit 8629bfcd284d9508974703ffb52ab374a57e3311, reversing
    changes made to 8ccf310a6231b89d5988084244a31d4e8c330ea7.

--- admin/eslint.config.js
+++ admin/eslint.config.js
@@ -1,37 +1,29 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default [
-  // Ignore dist folder
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    ignores: ["dist"],
-  },
-  // Base config
-  {
-    files: ["**/*.{js,jsx}"],
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
+        sourceType: 'module',
       },
     },
     rules: {
-      // Include recommended rules manually
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-
-      // Your custom rules
-      "react-refresh/only-export-components": "warn",
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
-];
+])
