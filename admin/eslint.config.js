64a55c16d457dbae2b8b8ce8771309b631cdf32d
mohamedhamdy9966 <mh7008002@gmail.com>
2026-04-24 18:14:58 +0300

    Revert "Merge pull request #162 from mohamedhamdy9966/development"
    This reverts commit f7e0a6799c38fbcd684b05e0d2fb4ebc22a621bc, reversing
    changes made to 2da85b872cb685a5b49689e7c2d21752409bba98.

--- admin/eslint.config.js
+++ admin/eslint.config.js
@@ -1,33 +1,29 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default [
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    ignores: ["dist", "dev-dist"],
-  },
-  {
-    files: ["**/*.{js,jsx}"],
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
+        sourceType: 'module',
       },
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
     rules: {
-      ...js.configs.recommended.rules,
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "react-refresh/only-export-components": "warn",
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
-];
+])
